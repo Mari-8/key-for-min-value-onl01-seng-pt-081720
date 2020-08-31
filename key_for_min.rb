e7363c74aa4502745f39af2e297fd98c7d9f04da
Ryan Altimari <ryanaltimari8@gmail.com>
2020-08-31 06:10:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,10 +9,21 @@ def key_for_min_value(name_hash)
   min_key =  nil
  
 name_hash.collect do |key, value|
+<<<<<<< HEAD
 	if min_val == nil || value < min_val
       min_val = value 
       min_key = key
     end 
   end
+=======
+	if min_val == nil
+      min_val = value
+    end 
+  if value < min_val 
+    min_val = value
+    min_key = key
+  end 
+end
+>>>>>>> f3f507f6274024edf190d5aef7c6d86c79d7f0ef
  min_key
  end
